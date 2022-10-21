f56d28cb2e45d5ffde945bcfe7460c552654df51
Stanleyrays <okoromstanleyrays@gmail.com>
2022-10-21 10:16:12 -0700

    task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-* main - main function
+*main - main function
 *
-* Return: nothing
+*Return: nothing
 */
 
 int main(void)
@@ -11,8 +11,10 @@ int counter = 2;
 long int a = 1;
 long int b = a + 1;
 long int c = a + b;
+ 
 printf("%ld, %ld, ", a, b);
 while (counter < 50)
+{
 printf("%ld", c);
 counter++;
 a = b;
