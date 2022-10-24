acbe81321479bb9a2e1128566eb1da0ae636b2b0
Stanleyrays <okoromstanleyrays@gmail.com>
2022-10-23 18:20:56 -0700

    task 1

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
-* Return: 0
+*main - causes an infinite loop
+*Return: 0
 */
 
 int main(void)
@@ -14,9 +14,9 @@ printf("Infinite loop incoming :(\n");
 i = 0;
 
 /**
-* main - avoiding a loop
-* Return: 0
-* while (i < 10)
+*main - avoiding a loop
+*Return: 0
+*while (i < 10)
 */
 int main(void)
 {
